ad2617ac9f65c14bbf58ffed48f06f2e7dffe2b6
Jenna Perez <jmsp43@gmail.com>
2023-05-11 14:05:34 -0400

    list view of all pokemon in browser

--- views/Index.jsx
+++ views/Index.jsx
@@ -3,21 +3,22 @@ const React = require("react");
 const styling = {
     color: '#ffffff',
     backgroundColor: '#aaaaaa',
+    padding: '15px',
 }
 
 
 class Index extends React.Component {
   render() {
       const { pokemon } = this.props;
-      console.log(pokemon)
     return (
       <div style = {styling}>
         <h1>Poke-Index Page</h1>
         <ul>
-          {pokemon.map((poke, i) => {
+                {pokemon.map((poke, i) => {
+                        let capName = poke.name.charAt(0).toUpperCase() + poke.name.slice(1)
             return (
               <li>
-                <a href={`/pokemon/${i}`}>{poke.name}</a>
+                <a href={`/pokemon/${i}`}>{capName}</a>
                 <br />
               </li>
             );
